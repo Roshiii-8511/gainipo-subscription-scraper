014652b63ed1ecc9338eea81be05df3475cd8b29
Roshiii-8511 <114097856+Roshiii-8511@users.noreply.github.com>
2025-12-17 12:30:25 +0530

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,1 +1,17 @@
-####
+from datetime import time
+
+# Market hours (IST)
+MARKET_START = time(10, 0)
+MARKET_END = time(17, 30)
+
+# BSE subscription URL
+BSE_SUBSCRIPTION_URL = (
+    "https://www.bseindia.com/markets/publicIssues/"
+    "CummDemandSchedule.aspx?ID={ipo_id}&status=L"
+)
+
+# NSE subscription URL (SME only)
+NSE_SUBSCRIPTION_URL = (
+    "https://www.nseindia.com/market-data/"
+    "issue-information?symbol={symbol}&series=EQ&type=Active"
+)
